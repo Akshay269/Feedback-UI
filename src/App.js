fa4908b0ecdaa9e58888740de89af088d375fe53
Akshay <aksrahangdale@gmail.com>
2022-05-30 16:01:19 +0530

    2ndcmt

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   return (
     <>
-      <Header />
+
+      <Header/>
       <div className="container">
         {/* <h1>My App</h1> */}
         <FeedbackList feedback={feedback}/>
+      
       </div>
+
     </>
   )
 }
