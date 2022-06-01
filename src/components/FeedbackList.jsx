740a4cea36f8884d4aca69a5d7270a8af6ae1c8a
Akshay <aksrahangdale@gmail.com>
2022-06-01 07:27:26 +0530

    5th cmt

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,28 @@
+import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback,handleDelete }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) return <p>No Feedback yet</p>;
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete} //as a prop
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete} //as a prop
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
